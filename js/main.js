1e50c7fef42b9395a7d6f46e0e2edec36f9495fc
unknown <davidbanu@ymail.com>
2018-10-21 17:47:40 +0000

    adding border and margins + take val for salary

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// JS resource
+$(document).ready(function() {
+$('#calc-salary').on("click", function(){
+    calculateSalary();
+});
+
+function calculateSalary() {
+var salariu  = $("#salariu").val();
+console.warn(salariu);
+}
+
+});
