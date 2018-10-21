363c011084aee9d700bf2a94d5cc646afec25432
unknown <davidbanu@ymail.com>
2018-10-21 19:32:41 +0000

    calcul salariu + afisare salariu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 $(document).ready(function() {
 $('#calc-salary').on("click", function(){
-    calculateSalary();
+    init();
 });
 
-function calculateSalary() {
+function init() {
 var salariu  = $("#salariu").val();
 console.warn(salariu);
+if (salariu.trim() === '' || salariu.trim() < 1900) {
+     $('#err-salariu').removeClass('hide').addClass('show');
+     $('#err-salariu').text('Eroare: Adaugati o valoare valida!')
+}
+else if (salariu.trim() >= 1900) {
+    $('#ecran-init').html('');
+    calculSalariu(salariu);
+}
+}
+
+function calculSalariu(salariuBrut) {
+var cas = ( salariuBrut/100 ) * 25;
+var cass = (salariuBrut/100) * 10;
+var iv = ((salariuBrut- cas -cass)/100) * 10;
+var salariuNet = salariuBrut - cas - cass - iv;
+afisareSalariu(salariuBrut, cas, cass, iv, salariuNet);
+}
+
+function afisareSalariu (salariuBrut, cas, cass, iv, salariuNet) {
+    $('#ecran-init').html('<p>Salariul brut este: ' + salariuBrut + '</p><br>' +
+    '<p CAS ul este de: ' + cas + '</p><br>' +
+    '<p> CASS ul este de: ' + cass + '</p><br>' +
+    '<p> IV ul este de: ' + iv + '</p><br>' +
+    '<p>Salariul net este de: ' + salariuNet + '</p>');
+
 }
 
 });
